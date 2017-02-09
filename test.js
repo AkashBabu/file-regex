e19a588554454f174204b09c1deab70524d08e23
Akashbabu256 <001akashbabu@gmail.com>
2017-02-09 17:51:10 +0530

    Added folder and script for testing

---
+++ test.js
@@ -0,0 +1,14 @@
+var findFiles = require('./index');
+
+
+findFiles(__dirname, "in.*", function(err, files){
+  console.log('no recursion', err, files);
+ })
+
+findFiles(__dirname, 'in.*', true, function(err, files) {
+  console.log('recursive file search without depth', err, files);
+})
+
+findFiles(__dirname, 'in.*', true, 2, function(err, files) {
+  console.log('recursive file search with depth', err, files);
+})
